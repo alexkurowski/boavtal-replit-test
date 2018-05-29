4b886fb687ba1de8416fa6179dd76e902afa9492
Alex <kurowski.dev@gmail.com>
2018-05-30 02:13:42 +0300

    Remove turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,6 @@
 //= require jquery
 //= require jquery-ui
 //= require jquery_ujs
-//= require turbolinks
 
 //= require parsleyjs/dist/parsley.min
 //= require inputmask/dist/jquery.inputmask.bundle
